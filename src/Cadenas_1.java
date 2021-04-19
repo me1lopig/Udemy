65a84cb4297cb8c12b8fdd0068401a9b482fb9c5
German Lopez Pineda <me1lopig@uco.es>
2021-04-19 21:41:46 +0200

    20210419

---
+++ src/Cadenas_1.java
@@ -0,0 +1,10 @@
+public class Cadenas_1 {
+// uso elemental de cadenas en Java
+    public static void main(String[] args) throws Exception {
+        String nombre="Juan";
+
+        System.out.println("Mi nombre es "+" "+nombre);
+        System.out.printf("La longitud de %s es %d%n",nombre,nombre.length());
+    }
+    
+}

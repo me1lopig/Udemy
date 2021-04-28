340d95685ee2b491e5c79f557b598f99f26f31b2
German Lopez Pineda <me1lopig@uco.es>
2021-04-28 22:54:04 +0200

    Comparacion de cadenas

--- src/Cadenas_1.java
+++ src/Cadenas_1.java
@@ -2,6 +2,7 @@ public class Cadenas_1 {
 // uso elemental de cadenas en Java
     public static void main(String[] args) {
         String nombre="Juan";
+        String nombre2="juan";
 
         System.out.println("Mi nombre es "+" "+nombre);
         System.out.printf("La longitud de %s es %d%n",nombre,nombre.length()); // ver la longitud de una cadena
@@ -14,6 +15,16 @@ public class Cadenas_1 {
         {
             System.out.printf("La posición %d de %s es %s%n",i,nombre,nombre.charAt(i));
         }
+        // comparar dos cadenas
+
+        // considerando las mayusculas
+        System.out.println("La comparación considerando mayusculas nos da "+nombre.equals(nombre2));
+        // sin considerar las mayusculas
+        System.out.println("La comparación son considerar mayusculas nos da "+nombre.equalsIgnoreCase(nombre2));
+
+        System.out.println(4<1);
+
+
     }
     
 }

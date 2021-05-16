2145cf550b047e2924aa0ebe0a061f1213ff9f90
German Lopez Pineda <me1lopig@uco.es>
2021-05-16 22:08:51 +0200

    20210515_funciona para cualquier divisor

--- src/es/udemy/calculos/DivisibleSiete.java
+++ src/es/udemy/calculos/DivisibleSiete.java
@@ -1,11 +1,28 @@
 package es.udemy.calculos;
 
+import java.util.Scanner;
 
 public class DivisibleSiete {
     public static void main(String[] args) {
-        int numero=14;
-        int numero_2=5;
-        System.out.printf("El numero es %d%n",numero%numero_2);
+        Scanner sc=new Scanner(System.in);
+
+        int numero;
+        int divisor;
+        int paso;
+
+        // entrada de datos
+        System.out.println("Introducir el mumero divisor ");
+        divisor=sc.nextInt();             
+        
+        System.out.println("Introducir el mumero base a sumar ");
+        numero=sc.nextInt();
+
+        paso=divisor*(numero/divisor+1);
+
+        // cálculo y resultados
+        System.out.printf("Hay que sumar %d a %d para obtener un multiplo de %d que es %d%n",paso-numero,numero,divisor,paso);
+
+        sc.close(); // cerramos el Scanner
     }
     
 }

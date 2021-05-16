2968224c5f2a5a21bcdba0e6207f4ae035ca7cfd
German Lopez Pineda <me1lopig@uco.es>
2021-05-16 17:23:50 +0200

    20210515_ numeros divisibles por 7

---
+++ src/es/udemy/calculos/DivisibleSiete.java
@@ -0,0 +1,11 @@
+package es.udemy.calculos;
+
+
+public class DivisibleSiete {
+    public static void main(String[] args) {
+        int numero=14;
+        int numero_2=5;
+        System.out.printf("El numero es %d%n",numero%numero_2);
+    }
+    
+}

7895d740b4febb3a22718cf2eabc2ce8513bcbdb
Germán López Pineda <me1lopig@uco.es>
2021-07-02 08:25:05 +0200

    20210702

--- src/es/udemy/MecanicaSuelos/cargasTerreno.java
+++ src/es/udemy/MecanicaSuelos/cargasTerreno.java
@@ -1,6 +1,8 @@
 // clase de cargas transmitidas al terreno por distintos tipos de cimenataciones
 // incluye los tipos
-// tensiones verticales de una carga rectangular
+// tensiones verticales de una carga rectangular calculo en el borde del rectangulo BxL
+// B es el dado menor
+// L es el lado mayor
 
 package es.udemy.MecanicaSuelos;
 
@@ -14,6 +16,19 @@ public class cargasTerreno {
     private double q; 
     // prpfundidad de calculo
     private double zmax=2*B; // se recomienda un valor inicial de 2 el ancho menor de la cimentación
+
+    public cargasTerreno(double B, double L,double q){
+        //Constructor asignacion de los valores de la geometroa dela zapata
+        this.B=B;
+        this.L=L;
+        this.q=q;
+
+    }
+
+    public void setProfundidad( double zmax){
+        // modificación de la profundidad de cálculo
+        this.zmax=zmax;
+    }
 
 
     

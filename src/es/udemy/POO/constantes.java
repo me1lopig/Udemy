fb69f6f874f4d9f5f8380d234c79bd7188de6249
German <glopez@ciccp.es>
2021-08-12 13:30:36 +0200

    20210811

---
+++ src/es/udemy/POO/constantes.java
@@ -0,0 +1,36 @@
+package es.udemy.POO;
+// uso de static
+
+public class constantes {
+
+    public static void main(String[] args) {
+        Empleados empleado1=new Empleados("Ana");
+        System.out.println(empleado1.getDatosEmpleado());
+
+    }
+    
+}
+
+class Empleados{
+
+    public Empleados (String nombre){
+        this.nombre=nombre;
+        seccion="Administracion";
+    }
+
+    public void setSeccion(String seccion){
+        this.seccion=seccion;
+    }
+
+    public String getDatosEmpleado(){
+
+        return "El empleado " + nombre + " pertenece a la seccion de " + seccion;
+    }
+
+
+    private String nombre;
+    private String seccion;
+
+
+
+}

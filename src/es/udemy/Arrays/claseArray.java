b24c7e85965d6a6ebbd2bb11ec7acf78ddbc281d
German <glopez@ciccp.es>
2021-08-18 19:28:54 +0200

    20210812

--- src/es/udemy/Arrays/claseArray.java
+++ src/es/udemy/Arrays/claseArray.java
@@ -8,20 +8,31 @@ public class claseArray {
     private double datos[];
     private int id;
     private static int control=0;
+    private static String nombres[]; // array estatico
+
 
     public claseArray(int numeroDatos) {
     // constructor para la creacion del vector
     this.numeroDatos=numeroDatos;
     datos=new double[numeroDatos];
+    nombres=new String [numeroDatos];
     id=0;
+    control++;
     }
 
    public void setIntroduceDatos(double valor){
         //Introducimos datos en el vector que se ha creado
         datos[id]=valor;
-        id++;
-        control++;
+        id++;       
     }
+
+    public void setIntroduceNombres(String nombre){
+        //Introducimos datos en el vector que se ha creado
+        nombres[id]=nombre;
+        id++;
+    }
+
+
 
     public void getImprimeDatos(){
         // imprimir los datos almacenados
@@ -29,6 +40,15 @@ public class claseArray {
             System.out.println("El valor del id="+i+" es "+datos[i]);
         }         
     }
+
+    public void getImprimeNombres(){
+        // imprimir los datos almacenados
+        for(int i=0;i<numeroDatos;i++){
+            System.out.println("El valor del id="+i+" es "+nombres[i]);
+        }         
+    }
+
+
 
     public void getImprimeDato(int posicion){
         // imprime un dato en concreto
@@ -43,7 +63,7 @@ public class claseArray {
     }   
 
         public int getNumeroDatos(){
-            
+
             // retornamos el mumero de datos introducidos en total
             return control;
 
--- src/es/udemy/Arrays/introduceDatos.java
+++ src/es/udemy/Arrays/introduceDatos.java
@@ -11,13 +11,21 @@ public class introduceDatos {
         claseArray vector_3=new claseArray(10);
     
 
-        // introduc¡mos los datos en el vector de la clase
+        // introduc¡mos los datos en el vector de datos
         for (int i=0;i<5;i++)
             {
                 vector_2.setIntroduceDatos(i+2);
             }
+     
+        // introduc¡mos los datos en el vector nombres
+        for (int i=0;i<5;i++)
+            {
+                vector_2.setIntroduceNombres("Nombre "+i);
+            }
+            
 
-        vector_2.getImprimeDatos(); // imprime todos los datos  que tenemos en el vector que hemos creados
+            vector_2.getImprimeDatos(); // imprime todos los datos  que tenemos en el vector que hemos creados
+            vector_2.getImprimeNombres(); // imprime todos los noombres  que tenemos en el vector que hemos creados
 
         // introduc¡mos los datos en el vector de la clase
         for (int i=0;i<10;i++)
@@ -25,8 +33,21 @@ public class introduceDatos {
                 vector_3.setIntroduceDatos(i*10);
             }
 
+        
+                // introduc¡mos los datos en el vector nombres
+        for (int i=0;i<10;i++)
+            {
+                vector_3.setIntroduceNombres("Nombre "+i);
+            }
+        
+        
+        
         vector_3.getImprimeDatos(); // imprime todos los datos  que tenemos en el vector que hemos creados
+        vector_3.getImprimeNombres(); // imprime todos los noombres  que tenemos en el vector que hemos creados
 
+       
+       
+       
         // sacamos datos parciales de los arrays creados
         System.out.println("primer ejemplo");
         vector_2.getImprimeDato(2);

0ce03fa0d576fe0a4b016f9c088ddba5334ddae1
German Lopez Pineda <me1lopig@uco.es>
2021-08-29 18:48:49 +0200

    20210829

--- src/es/udemy/POO/UsoColegio.java
+++ src/es/udemy/POO/UsoColegio.java
@@ -8,20 +8,32 @@ public class UsoColegio {
     public static void main(String[] args) {
 
         Colegio SanJavier = new Colegio("San Javier",200);
+        Colegio SanJuan = new Colegio("San Juan",150);
     
         
-        // introducción de datos de los alumnos
-        SanJavier.nuevoAlumno("Antonio", 7);
-        SanJavier.nuevoAlumno("Manuela", 8);
-        SanJavier.nuevoAlumno("Juan", 6);
+        // introducción de datos de los alumnos de un colegio 1
+        SanJavier.nuevoAlumno("Antonio", 7.9);
+        SanJavier.nuevoAlumno("Manuela", 8.3);
+        SanJavier.nuevoAlumno("Juan", 6.8);
+
+        // introducción de datos de los alumnos de un colegio 2
+        SanJuan.nuevoAlumno("Manuel", 7.8);
+        SanJuan.nuevoAlumno("Josefina", 8.9);
+        SanJuan.nuevoAlumno("Antonia", 6.7);
+  
+  
 
         // obtención de los datos de todos loa alumnos incluidos
-        SanJavier.getTodosAlumnos();
+
         
         // obtención de un alumno en particular
 
 
         // cambiar la nota de algún alumno
+
+        // imprine el código final mediante un método estático
+        
+        System.out.println("El código final es "+Colegio.getcodigoFinal());
 
 
     }
